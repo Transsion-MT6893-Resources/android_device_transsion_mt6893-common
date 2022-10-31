f0c39ba7ccaa41b129f4482a0318e232f1466d07
LuK1337 <priv.luk@gmail.com>
2022-10-31 21:59:52 +0100

    mt6893-common: overlay: Configure aux camera for Aperture
    Change-Id: I543f078dd4dba30e1b22737d5c22d591e3fb34e6

--- common.mk
+++ common.mk
@@ -233,6 +233,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
+    ApertureResCommon \
     FrameworksResCommon \
     SettingsResCommon \
     SystemUIResCommon \
