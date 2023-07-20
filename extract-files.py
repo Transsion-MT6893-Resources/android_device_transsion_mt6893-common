191503a15094e423112c01a221285eac71cf9031
SamarV-121 <samarvispute121@pm.me>
2023-07-20 14:52:21 +0100

    mt6893-common: Link older libutils for PQ HAL
    * Abort message: 'incStrongRequireStrong() called on 0xb400007c5afa7250 which isn't already owned'
    
    Change-Id: I7ea86d3d7c68ba183b4838cb1f35a9fc58cfbf67

--- extract-files.py
+++ extract-files.py
@@ -4,10 +4,19 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
 )
+
+blob_fixups: blob_fixups_user_type = {
+    ('vendor/lib/hw/vendor.mediatek.hardware.pq@2.15-impl.so', 'vendor/lib64/hw/vendor.mediatek.hardware.pq@2.15-impl.so'): blob_fixup()
+        .replace_needed('libutils.so', 'libutils-v32.so'),
+}   # fmt: skip
 
 namespace_imports = [
     'device/transsion/mt6893-common',
@@ -18,6 +27,7 @@ namespace_imports = [
 module = ExtractUtilsModule(
     'mt6893-common',
     'transsion',
+    blob_fixups=blob_fixups,
     namespace_imports=namespace_imports,
 )
 
