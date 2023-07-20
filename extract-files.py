e0cec6b35c8f5df1f48ae3a02e544caa0c597c46
Jiyong Park <jiyong@google.com>
2023-07-20 19:38:06 +0100

    mt6893-common: power: Remove ndk_platform backend. Use the ndk backend.
    The ndk_platform backend will soon be deprecated because the ndk backend
    can serve the same purpose. This is to eliminate the confusion about
    having two variants (ndk and ndk_platform) for the same ndk backend.
    
    Bug: 161456198
    Test: m
    Merged-In: Id16039722fd70d4e2e934a0988f13a768ce833ec
    Change-Id: Id16039722fd70d4e2e934a0988f13a768ce833ec

--- extract-files.py
+++ extract-files.py
@@ -14,6 +14,8 @@ from extract_utils.main import (
 )
 
 blob_fixups: blob_fixups_user_type = {
+    'vendor/bin/hw/vendor.mediatek.hardware.mtkpower@1.0-service': blob_fixup()
+        .replace_needed('android.hardware.power-V2-ndk_platform.so', 'android.hardware.power-V2-ndk.so'),
     ('vendor/lib/hw/vendor.mediatek.hardware.pq@2.15-impl.so', 'vendor/lib64/hw/vendor.mediatek.hardware.pq@2.15-impl.so'): blob_fixup()
         .replace_needed('libutils.so', 'libutils-v32.so'),
 }   # fmt: skip
