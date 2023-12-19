2f3da7903cf618cb39cce7997c3db60adeee0048
Woomymy <woomy@woomy.be>
2023-12-19 16:09:53 +0000

    mt6893-common: Link media.c2 service against v33 libstagefright_foundation
    * Fixes crashes and high CPU usage
    
    Change-Id: I60c07d10c2db9335d20c6dbaaaeacc9313aebf06
    Signed-off-by: Woomymy <woomy@woomy.be>

--- extract-files.py
+++ extract-files.py
@@ -18,6 +18,8 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('android.hardware.light-V1-ndk_platform.so', 'android.hardware.light-V1-ndk.so'),
     ('vendor/bin/hw/android.hardware.gnss-service.mediatek', 'vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so'): blob_fixup()
         .replace_needed('android.hardware.gnss-V1-ndk_platform.so', 'android.hardware.gnss-V1-ndk.so'),
+    ('vendor/bin/hw/android.hardware.media.c2@1.2-mediatek', 'vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b'): blob_fixup()
+        .add_needed('libstagefright_foundation-v33.so'),
     'vendor/bin/hw/vendor.mediatek.hardware.mtkpower@1.0-service': blob_fixup()
         .replace_needed('android.hardware.power-V2-ndk_platform.so', 'android.hardware.power-V2-ndk.so'),
     ('vendor/lib/hw/vendor.mediatek.hardware.pq@2.15-impl.so', 'vendor/lib64/hw/vendor.mediatek.hardware.pq@2.15-impl.so'): blob_fixup()
