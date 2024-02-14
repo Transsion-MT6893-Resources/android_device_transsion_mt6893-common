b59b1bca6f74165810c5a947802c97b807ddfb39
alextrack2013 <alextrack2013@gmail.com>
2024-02-14 12:27:48 +0100

    mt6893-common: Switch to AOSP AIDL Boot HAL
    Change-Id: I74698c395a7a5c64bdc44d2a99a251eeb5a3519e
    Signed-off-by: alextrack2013 <alextrack2013@gmail.com>

--- common.mk
+++ common.mk
@@ -33,9 +33,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/userspace_reboot.mk)
 
 # Boot control
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-mtkimpl \
-    android.hardware.boot@1.2-mtkimpl.recovery \
-    android.hardware.boot@1.2-service
+    android.hardware.boot-service.default \
+    android.hardware.boot-service.default_recovery
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
