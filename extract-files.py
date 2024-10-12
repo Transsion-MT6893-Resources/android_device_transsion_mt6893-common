5a55de489fd813396dc8232be395561d58c679a2
Cosmin Tanislav <demonsingur@gmail.com>
2024-10-12 21:16:44 +0530

    mt6893-common: automatically patch ims
    https://review.lineageos.org/q/topic:%22extract-utils-apktool%22
    
    Change-Id: I704e191adc7aafe6fe062c1bb330fbd1ca05493c

--- extract-files.py
+++ extract-files.py
@@ -33,6 +33,8 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
+    'system_ext/priv-app/ImsService/ImsService.apk': blob_fixup()
+        .apktool_patch('patches/ims'),
     'system_ext/lib64/libsink.so': blob_fixup()
         .add_needed('libaudioclient_shim.so'),
     'system_ext/lib64/libsource.so': blob_fixup()
