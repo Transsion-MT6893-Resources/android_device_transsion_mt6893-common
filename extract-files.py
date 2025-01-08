b45e85b90d831b3487f71b3e5a5c53b42fa3e53f
Giovanni Ricca <giovanniricca@proton.me>
2025-01-08 12:55:07 +0100

    mt6893-common: `libshim_sink.so` -> `libaudioclient_shim.so`
    Change-Id: I27c7cea3f2b7feda9736b9f1b1aa52db9202736e

--- extract-files.py
+++ extract-files.py
@@ -15,7 +15,7 @@ from extract_utils.main import (
 
 blob_fixups: blob_fixups_user_type = {
     'system_ext/lib64/libsink.so': blob_fixup()
-        .add_needed('libshim_sink.so'),
+        .add_needed('libaudioclient_shim.so'),
     'vendor/bin/factory': blob_fixup()
         .replace_needed('android.hardware.light-V1-ndk_platform.so', 'android.hardware.light-V1-ndk.so'),
     ('vendor/bin/hw/android.hardware.gnss-service.mediatek', 'vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so'): blob_fixup()
