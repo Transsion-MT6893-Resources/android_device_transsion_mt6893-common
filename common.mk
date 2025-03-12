22b00b4dd507f7af39b35fe7af88ea8a35f2fe43
dodyirawan85 <dev.irawans@gmail.com>
2025-03-12 21:43:01 +0700

    mt6893-common: Configure gatekeeper stack
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- common.mk
+++ common.mk
@@ -62,6 +62,11 @@ PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.1-impl-mock \
     fastbootd
 
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-service
+
 # Include GSI keys
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
